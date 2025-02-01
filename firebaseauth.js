25a223560bb9041f48e1f0ab3d59026ecdec312e
zahra mohammadi <zahramohamadi257@gmail.com>
2025-02-01 13:01:29 +0330

    Add prettier

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,119 +1,120 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-  
-  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,signInAnonymously  } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInAnonymously,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
-  if(localStorage.getItem("loggedInUserId")){
-    window.location.href = "todopapge.html"; 
-  }
-  const firebaseConfig = {
-    apiKey: "AIzaSyBNMvZvFc-cBYSRBhqWM7z0Nc_dQi-b9Nk",
-    authDomain: "todolist-zahra.firebaseapp.com",
-    databaseURL: "https://todolist-zahra-default-rtdb.firebaseio.com",
-    projectId: "todolist-zahra",
-    storageBucket: "todolist-zahra.firebasestorage.app",
-    messagingSenderId: "298438939395",
-    appId: "1:298438939395:web:3569d62b57d1de5fbc8f36",
-    measurementId: "G-SCL03Q23KB"
-  };
+if (localStorage.getItem("loggedInUserId")) {
+  window.location.href = "todopapge.html";
+}
+const firebaseConfig = {
+  apiKey: "AIzaSyBNMvZvFc-cBYSRBhqWM7z0Nc_dQi-b9Nk",
+  authDomain: "todolist-zahra.firebaseapp.com",
+  databaseURL: "https://todolist-zahra-default-rtdb.firebaseio.com",
+  projectId: "todolist-zahra",
+  storageBucket: "todolist-zahra.firebasestorage.app",
+  messagingSenderId: "298438939395",
+  appId: "1:298438939395:web:3569d62b57d1de5fbc8f36",
+  measurementId: "G-SCL03Q23KB",
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
-  function showMessage(message, divId){
-    var messageDiv=document.getElementById(divId);
-    messageDiv.style.display="block";
-    messageDiv.innerHTML=message;
-    messageDiv.style.opacity=1;
-    setTimeout(function(){
-        messageDiv.style.opacity=0;
-    },5000);
- }
+function showMessage(message, divId) {
+  var messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = 1;
+  setTimeout(function () {
+    messageDiv.style.opacity = 0;
+  }, 5000);
+}
 
- const anonymousBtn = document.getElementById("anonymousLogin");
+const anonymousBtn = document.getElementById("anonymousLogin");
 
 anonymousBtn.addEventListener("click", () => {
   signInAnonymously(auth)
     .then((userCredential) => {
       const user = userCredential.user;
       console.log("Anonymous user signed in:", user.uid);
-      localStorage.setItem("loggedInUserId", user.uid); 
-      window.location.href = "todopapge.html"; 
+      localStorage.setItem("loggedInUserId", user.uid);
+      window.location.href = "todopapge.html";
     })
     .catch((error) => {
       console.error("Error signing in anonymously:", error);
       showMessage("Unable to login anonymously", "signInMessage");
     });
 });
- 
 
-  const signUp=document.getElementById('submitSignUp');
-  signUp.addEventListener('click', (event)=>{
+const signUp = document.getElementById("submitSignUp");
+signUp.addEventListener("click", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("rEmail").value;
+  const password = document.getElementById("rPassword").value;
+  const firstName = document.getElementById("fName").value;
+  const lastName = document.getElementById("lName").value;
 
-    event.preventDefault();
-    const email=document.getElementById('rEmail').value;
-    const password=document.getElementById('rPassword').value;
-    const firstName=document.getElementById('fName').value;
-    const lastName=document.getElementById('lName').value;
-
-    const auth=getAuth();
-    const db=getFirestore();
-    createUserWithEmailAndPassword(auth, email, password)
-
+  const auth = getAuth();
+  const db = getFirestore();
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        const user=userCredential.user;
-        const userData={
-            email: email,
-            firstName: firstName,
-            lastName:lastName
-        };
-        showMessage('Account Created Successfully', 'signUpMessage')
-        const docRef=doc(db, "users", user.uid);
-        setDoc(docRef,userData)
+      const user = userCredential.user;
+      const userData = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+      };
+      showMessage("Account Created Successfully", "signUpMessage");
+      const docRef = doc(db, "users", user.uid);
+      setDoc(docRef, userData)
         .then(() => {
-            window.location.href='index.html';
+          window.location.href = "index.html";
         })
-        .catch((error)=>{
-            console.error("error writing document", error);
-
+        .catch((error) => {
+          console.error("error writing document", error);
         });
     })
-    .catch((error)=>{
-        const errorCode=error.code;
-        if(errorCode=='auth/email-already-in-use'){
-            showMessage('Email Address Already Exists', 'signUpMessage');
-        }
-        else{
-            showMessage('unable to create User', 'signUpMessage');
-        }
-    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode == "auth/email-already-in-use") {
+        showMessage("Email Address Already Exists", "signUpMessage");
+      } else {
+        showMessage("unable to create User", "signUpMessage");
+      }
+    });
+});
 
-  })
+const signIn = document.getElementById("submitSignIn");
+signIn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const auth = getAuth();
 
-  const signIn=document.getElementById('submitSignIn');
-  signIn.addEventListener('click', (event)=>{
-    event.preventDefault();
-    const email=document.getElementById('email').value;
-    const password=document.getElementById('password').value;
-    const auth=getAuth();
-
-    signInWithEmailAndPassword(auth, email,password)
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        showMessage('login is successful', 'signInMessage');
-        const user=userCredential.user;
-        localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='todopapge.html';
+      showMessage("login is successful", "signInMessage");
+      const user = userCredential.user;
+      localStorage.setItem("loggedInUserId", user.uid);
+      window.location.href = "todopapge.html";
     })
 
     .catch((error) => {
-        const errorCode=error.code;
-        if(errorCode==='auth/invalid-credential'){
-            showMessage('Incorrect Email or Password', 'signInMessage');
-        }
-        else{
-            showMessage('Account does not Exist', 'signInMessage');
-        }
-    })
-  })
+      const errorCode = error.code;
+      if (errorCode === "auth/invalid-credential") {
+        showMessage("Incorrect Email or Password", "signInMessage");
+      } else {
+        showMessage("Account does not Exist", "signInMessage");
+      }
+    });
+});
--- todopapge.js
+++ todopapge.js
@@ -1,97 +1,97 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-  
-import { getAuth, onAuthStateChanged, signOut ,signInAnonymously} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+  signInAnonymously,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import {
+  getFirestore,
+  getDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNMvZvFc-cBYSRBhqWM7z0Nc_dQi-b9Nk",
-    authDomain: "todolist-zahra.firebaseapp.com",
-    databaseURL: "https://todolist-zahra-default-rtdb.firebaseio.com",
-    projectId: "todolist-zahra",
-    storageBucket: "todolist-zahra.firebasestorage.app",
-    messagingSenderId: "298438939395",
-    appId: "1:298438939395:web:3569d62b57d1de5fbc8f36",
-    measurementId: "G-SCL03Q23KB"
-  };
+  apiKey: "AIzaSyBNMvZvFc-cBYSRBhqWM7z0Nc_dQi-b9Nk",
+  authDomain: "todolist-zahra.firebaseapp.com",
+  databaseURL: "https://todolist-zahra-default-rtdb.firebaseio.com",
+  projectId: "todolist-zahra",
+  storageBucket: "todolist-zahra.firebasestorage.app",
+  messagingSenderId: "298438939395",
+  appId: "1:298438939395:web:3569d62b57d1de5fbc8f36",
+  measurementId: "G-SCL03Q23KB",
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-  const auth=getAuth();
-  const db=getFirestore();
-
-  onAuthStateChanged(auth, (user)=>{
-    const loggedInUserId=localStorage.getItem('loggedInUserId');
-    if(loggedInUserId){
-        console.log(user);
-        const docRef = doc(db, "users", loggedInUserId);
-        getDoc(docRef)
-
-        .then((docSnap) => {
-            if(docSnap.exists()){
-                const userData=docSnap.data();
-                document.getElementById('loggedUserFName').innerText=userData.firstName;
-                document.getElementById('loggedUserEmail').innerText=userData.email;
-                document.getElementById('loggedUserLName').innerText=userData.lastName;
-            }else{
-                console.log("no document found matching id")
-            }
-        })
-        .catch((error) =>{
-            console.log("Error getting document");
-        })
-    }
-    else{
-        console.log("User Id not Found in Local storage")
-    }
-  })
-
-
-  const logoutButton=document.getElementById('logout');
-  logoutButton.addEventListener('click',()=>{
-    localStorage.removeItem('loggedInUserId');
-    signOut(auth)
-    .then(()=>{
-        window.location.href='index.html';
-    })
-    .catch((error)=>{
-        console.error('Error Signing out:', error);
-    })
-  })
-
-
-  const logintButton=document.getElementById('login');
-  logintButton.addEventListener('click',()=>{
-    localStorage.removeItem('loggedInUserId');
-    signOut(auth)
-    .then(()=>{
-        window.location.href='index.html';
-    })
-    .catch((error)=>{
-        console.error('Error Signing out:', error);
-    })
-  })
+const auth = getAuth();
+const db = getFirestore();
 
 onAuthStateChanged(auth, (user) => {
-    const logoutButton = document.getElementById('logout');
-    const loginButton = document.querySelector('#login');
-  
-    if (user) {
-      if (user.isAnonymous) {
-       
-        logoutButton.style.display = 'none';
-        loginButton.style.display = 'block'; 
-      } else {
-       
-        logoutButton.style.display = 'block'; 
-        loginButton.style.display = 'none';  
-      }
+  const loggedInUserId = localStorage.getItem("loggedInUserId");
+  if (loggedInUserId) {
+    console.log(user);
+    const docRef = doc(db, "users", loggedInUserId);
+    getDoc(docRef)
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          document.getElementById("loggedUserFName").innerText =
+            userData.firstName;
+          document.getElementById("loggedUserEmail").innerText = userData.email;
+          document.getElementById("loggedUserLName").innerText =
+            userData.lastName;
+        } else {
+          console.log("no document found matching id");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document");
+      });
+  } else {
+    console.log("User Id not Found in Local storage");
+  }
+});
+
+const logoutButton = document.getElementById("logout");
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUserId");
+  signOut(auth)
+    .then(() => {
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      console.error("Error Signing out:", error);
+    });
+});
+
+const logintButton = document.getElementById("login");
+logintButton.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUserId");
+  signOut(auth)
+    .then(() => {
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      console.error("Error Signing out:", error);
+    });
+});
+
+onAuthStateChanged(auth, (user) => {
+  const logoutButton = document.getElementById("logout");
+  const loginButton = document.querySelector("#login");
+
+  if (user) {
+    if (user.isAnonymous) {
+      logoutButton.style.display = "none";
+      loginButton.style.display = "block";
     } else {
-      
-      logoutButton.style.display = 'none'; 
-      loginButton.style.display = 'block'; 
+      logoutButton.style.display = "block";
+      loginButton.style.display = "none";
     }
-  });
-  
-
-
-  
+  } else {
+    logoutButton.style.display = "none";
+    loginButton.style.display = "block";
+  }
+});
